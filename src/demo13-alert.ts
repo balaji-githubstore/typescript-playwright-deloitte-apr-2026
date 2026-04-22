7cadf6389de48ef4799710c095aa174d10da1257
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-22 15:11:17 +0530

    alert

--- src/demo13-alert.ts
+++ src/demo13-alert.ts
@@ -7,8 +7,19 @@ const context = await browser.newContext();
 const page = await context.newPage();
 
 await page.goto("https://www.nasscom.in/nasscom-membership");
+//register the event to modify alert handling. 
+await page.on('dialog',async dialog=>{
+    console.log(dialog.message());
+    await dialog.accept();
+})
 
 //click on calculate fee
+await page.locator("xpath=//a[@id='calculate-fee']").click();
+
+// not working
+// const d=page.waitForEvent('dialog')
+// await page.locator("xpath=//a[@id='calculate-fee']").click();
+// const dialogue=await d;
 
 await page.waitForTimeout(5000)
 await browser.close()
