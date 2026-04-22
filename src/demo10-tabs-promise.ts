02e9389446705086529437240c10b756d308a106
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-22 12:03:35 +0530

    Mutliple tabs work

--- src/demo10-tabs-promise.ts
+++ src/demo10-tabs-promise.ts
@@ -8,11 +8,24 @@ const page = await context.newPage();
 
 await page.goto("https://www.online.citibank.co.in/");
 
+//check for presence of locator
+if (await page.locator("xpath=//button[@id='onetrust-accept-btn-handler']").count() == 1) {
+    await page.locator("xpath=//button[@id='onetrust-accept-btn-handler']").click()
+}
+
+
+
 //hover on //div[text()='My Account']
-//click on //div[text()='Banking with Citi'] will open new tab
+await page.locator("xpath=//div[text()='My Account']").hover()
+
+const [newPage,] = await Promise.all(
+    [page.waitForEvent('popup'),
+    page.locator("xpath=//div[text()='Banking with Citi']").click()
+    ])
 
 //in new tab
 //enter userid
+await newPage.locator("xpath=//input[@id='username']").fill("john")
 
 await page.waitForTimeout(5000)
 await browser.close()
