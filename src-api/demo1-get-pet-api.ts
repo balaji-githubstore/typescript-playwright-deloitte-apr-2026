4c784cb3e21acc18bdd305a0546890ad3d76afb7
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-22 17:29:31 +0530

    get api code update

--- src-api/demo1-get-pet-api.ts
+++ src-api/demo1-get-pet-api.ts
@@ -1,15 +1,17 @@
-import { request, type APIRequestContext, type APIResponse } from "playwright";
+import { request } from "playwright";
 
-const baseUrl:string="https://petstore.swagger.io/v2"
-const resource:string="/pet/10"
 
 const apiRequestContext= await request.newContext(); 
 
-const response=await apiRequestContext.get(baseUrl+resource)
+const response=await apiRequestContext.get("https://petstore.swagger.io/v2/pet/10")
 
 console.log(response.status())
+console.log(response.statusText())
 const body = await response.json()
 console.log(body)
 
 console.log(body.id)
 console.log(body.name)
+
+// console.log(await response.text())
+console.log(body.category.id)
