330779c1824ece21920959fa1c6a47e1ef3788f3
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-23 12:41:46 +0530

    Complete git get repo

--- src-api/demo7-git-get-repo-user-api.ts
+++ src-api/demo7-git-get-repo-user-api.ts
@@ -8,11 +8,11 @@ import { request } from "playwright";
 const baseUrl="https://api.github.com"
 const resource="/user/repos?visibility=private"
 
-// const token = read secret.json and store token
+const token = 'read secret.json and store token'
 
 const apiRequestContext= await request.newContext(); 
 const response=await apiRequestContext.get(baseUrl+resource,
-    {headers:{'Authorization':'Bearer ','Accept':'application/vnd.github+json'}}
+    {headers:{'Authorization':'Bearer '+token,'Accept':'application/vnd.github+json'}}
 )
 
 console.log(response.status())
