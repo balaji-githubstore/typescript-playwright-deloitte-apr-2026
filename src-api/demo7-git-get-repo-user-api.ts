d01a036ded74d36c0a4582ec8cda2f2d8de11de1
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-23 12:58:51 +0530

    git - private repo for auth user

--- src-api/demo7-git-get-repo-user-api.ts
+++ src-api/demo7-git-get-repo-user-api.ts
@@ -4,15 +4,26 @@
  */
 
 import { request } from "playwright";
+import fs from "node:fs";
 
-const baseUrl="https://api.github.com"
-const resource="/user/repos?visibility=private"
+const baseUrl = "https://api.github.com"
+const resource = "/user/repos?visibility=private"
 
-const token = 'read secret.json and store token'
+const secretJson=JSON.parse(
+    fs.readFileSync('test-data/secret.json','utf-8')
+)
 
-const apiRequestContext= await request.newContext(); 
-const response=await apiRequestContext.get(baseUrl+resource,
-    {headers:{'Authorization':'Bearer '+token,'Accept':'application/vnd.github+json'}}
+const apiRequestContext = await request.newContext();
+const response = await apiRequestContext.get(baseUrl + resource,
+    {
+        headers: {
+            'Authorization': 'Bearer ' + secretJson.token,
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2026-03-10'
+        }
+    }
 )
 
 console.log(response.status())
+
+//try to write logic to print full_name for each repo from the response
